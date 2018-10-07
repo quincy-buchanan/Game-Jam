1497378797d0adc968e538d256a907ff5ae9e2f7
quincy-buchanan <35669413+quincy-buchanan@users.noreply.github.com>
2018-10-06 20:28:34 -0400

    Work on TD

---
+++ Music TD/Assets/Scripts/BandSelect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BandSelect : MonoBehaviour {
+
+	public int tag;
+	public string genre;
+	public GameObject info;
+
+	public void ShowInfo()
+	{
+		info.SetActive(true);
+		//more to come...
+
+		GameObject.Find ("HireBandButton").GetComponent<Button> ().interactable = true;
+		GameObject.Find ("HireBandButton").GetComponent<HireBand> ().genre = genre;
+		GetComponent<Button> ().interactable = false;
+	}
+}
---
+++ Music TD/Assets/Scripts/HireBand.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HireBand : MonoBehaviour {
+
+	public int tag;
+	public string genre;
+	public GameObject venueMenu;
+
+	public void OpenVenueMenu()
+	{
+		venueMenu.SetActive (true);
+	}
+
+	public void SetVenueGenre()
+	{
+		if (GameObject.Find ("Venue1").GetComponent<HireBand> ().tag == tag) {
+			GameObject.Find ("VenueText1").GetComponent<Text> ().text = genre;
+		}
+
+		venueMenu.SetActive (false);
+	}
+}

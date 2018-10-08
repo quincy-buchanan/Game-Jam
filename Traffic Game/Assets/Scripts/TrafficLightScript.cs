079e9999dacf462cf7c26aab3081b15f21df675a
quincy-buchanan <35669413+quincy-buchanan@users.noreply.github.com>
2018-10-08 17:16:58 -0400

    Traffic Light Buttons

---
+++ Traffic Game/Assets/Scripts/TrafficLightScript.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrafficLightScript : MonoBehaviour {
+
+    public IntersectionScript intersection;
+
+    public bool leftLightOn;
+    public bool rightLightOn;
+    public bool topLightOn;
+    public bool bottomLightOn;
+
+    public string roadSide;
+
+    private void Start()
+    {
+        leftLightOn = intersection.leftLightOn;
+        rightLightOn = intersection.rightLightOn;
+        topLightOn = intersection.topLightOn;
+        bottomLightOn = intersection.bottomLightOn;
+
+        if (roadSide == "l")
+        {
+            ChangeColor(leftLightOn);
+        }
+        else if (roadSide == "r")
+        {
+            ChangeColor(rightLightOn);
+        }
+        else if (roadSide == "t")
+        {
+            ChangeColor(topLightOn);
+        }
+        else if (roadSide == "b")
+        {
+            ChangeColor(bottomLightOn);
+        }
+    }
+
+    public void ChangeLight()
+    {
+        if (roadSide == "l")
+        {
+            leftLightOn = !leftLightOn;
+            ChangeColor(leftLightOn);
+        }
+        else if (roadSide == "r")
+        {
+            rightLightOn = !rightLightOn;
+            ChangeColor(rightLightOn);
+        }
+        else if (roadSide == "t")
+        {
+            topLightOn = !topLightOn;
+            ChangeColor(topLightOn);
+        }
+        else if(roadSide == "b")
+        {
+            bottomLightOn = !bottomLightOn;
+            ChangeColor(bottomLightOn);
+        }
+    }
+
+    void ChangeColor(bool lightOn)
+    {
+        ColorBlock cb = GetComponent<Button>().colors;
+
+        if (lightOn)
+        {
+            cb.normalColor = Color.green;
+            cb.highlightedColor = new Color(0, .5f, 0, 1);
+            GetComponent<Button>().colors = cb;
+        }
+        else
+        {
+            cb.normalColor = Color.red;
+            cb.highlightedColor = new Color(.5f, 0, 0, 1);
+            GetComponent<Button>().colors = cb;
+        }
+    }
+}
